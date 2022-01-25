8d01faf90070c31dc959db84957595a8b6837318
time4ruin <time4_ruin@naver.com>
2022-01-25 03:03:21 -0800

    test made-up

---
+++ libgcrypt-1.9.4/test.sh
@@ -0,0 +1,24 @@
+A=$(which as)
+B=$(ls -l $A)
+C=${B#*-\> }
+
+ln -Tfs /usr/bin/x86_64-linux-gnu-as /usr/bin/as
+echo $(ls -al /usr/bin/as)
+make clean > /dev/null 2>&1
+make > /dev/null 2>&1
+echo $(ls -al ./tests/mpitests)
+StartTime=$(date +%s)
+./tests/mpitests
+EndTime=$(date +%s)
+echo "Total Execution Time: $(($EndTime - $StartTime)) seconds"
+
+ln -Tfs $C /usr/bin/as
+echo $(ls -al /usr/bin/as)
+make clean > /dev/null 2>&1
+make > /dev/null 2>&1
+echo $(ls -al ./tests/mpitests)
+StartTime=$(date +%s)
+./tests/mpitests
+EndTime=$(date +%s)
+echo "Total Execution Time: $(($EndTime - $StartTime)) seconds"
+
