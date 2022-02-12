b36fe6128ee61b77eb3944dae96457f22f1b0ae4
time4ruin <time4_ruin@naver.com>
2022-02-11 22:47:36 -0800

    run.sh

--- libgcrypt-1.9.4/run.sh
+++ libgcrypt-1.9.4/run.sh
@@ -7,18 +7,13 @@ echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
 echo $(ls -al ./tests/mpitests)
-StartTime=$(date +%s)
-./tests/mpitests
-EndTime=$(date +%s)
-echo "Total Execution Time: $(($EndTime - $StartTime)) seconds"
+echo $(ls -al ./mpi/.libs/mpi-pow.o)
+echo "abc123" | sudo -S perf stat ./tests/mpitests
 
 echo "abc123" | sudo -S ln -Tfs $C /usr/bin/as
 echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
 echo $(ls -al ./tests/mpitests)
-StartTime=$(date +%s)
-./tests/mpitests
-EndTime=$(date +%s)
-echo "Total Execution Time: $(($EndTime - $StartTime)) seconds"
-
+echo $(ls -al ./mpi/.libs/mpi-pow.o)
+echo "abc123" | sudo -S perf stat ./tests/mpitests
