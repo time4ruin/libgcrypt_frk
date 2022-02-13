8ea6d3ad0d9f689f2cc5f55bd3a253e9d721f748
time4ruin <time4_ruin@naver.com>
2022-02-12 23:19:03 -0800

    modify run.sh

--- libgcrypt-1.9.4/run.sh
+++ libgcrypt-1.9.4/run.sh
@@ -6,14 +6,14 @@ echo "abc123" | sudo -S ln -Tfs /usr/bin/x86_64-linux-gnu-as /usr/bin/as
 echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
-echo $(ls -al ./tests/mpitests)
-echo $(ls -al ./mpi/.libs/mpi-pow.o)
-echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/mpitests >/dev/null
+echo $(ls -al ./tests/t-ed25519)
+echo $(ls -al ./mpi/.libs/ec.o)
+echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/t-ed25519 >/dev/null
 
 echo "abc123" | sudo -S ln -Tfs $C /usr/bin/as
 echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
-echo $(ls -al ./tests/mpitests)
-echo $(ls -al ./mpi/.libs/mpi-pow.o)
-echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/mpitests >/dev/null
+echo $(ls -al ./tests/t-ed25519)
+echo $(ls -al ./mpi/.libs/ec.o)
+echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/t-ed25519 >/dev/null
